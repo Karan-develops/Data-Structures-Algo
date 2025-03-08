ee891b0c7fb8d572208a4e6f34ab4f0a76130ec7
Karan <mrkaran2k5@gmail.com>
2025-03-08 19:10:25 +0530

    Symmetric Tree

---
+++ Trees/questions/isSymmetric.cpp
@@ -0,0 +1,28 @@
+// LC - 101 - Easy
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* left;
+    Node* right;
+};
+class Solution {
+    bool solve(Node* leftSide,Node* rightSide){
+        if(leftSide==NULL && rightSide==NULL) return true;
+        if(leftSide==NULL || rightSide==NULL) return false;
+        return ((leftSide->val==rightSide->val) && 
+        solve(leftSide->left,rightSide->right) && 
+        solve(leftSide->right,rightSide->left));
+    }
+    public:
+    bool isSymmetric(Node* root) {
+        return solve(root->left,root->right);
+    }
+};
+int main(){
+    Solution sol;
+    // IP Tree [1,2,2,3,4,4,3]
+    // sol.isSymmetric(tree);
+    return 0;
+}
