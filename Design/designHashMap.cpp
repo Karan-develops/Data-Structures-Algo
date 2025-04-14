33f68f48cf41796a4875c07acb3fe8a264e53536
Karan <mrkaran2k5@gmail.com>
2025-04-14 10:48:29 +0530

    Design HashMap

---
+++ Design/designHashMap.cpp
@@ -0,0 +1,49 @@
+// LC - 706 - Easy - Design HashMap
+#include<iostream>
+#include<vector>
+#include<list>
+using namespace std;
+class HashMap{
+    int size=10000;
+    vector<list<pair<int,int>>>bucket;
+    public:
+    HashMap(){
+        bucket.resize(size);
+    }
+    int getHashIdx(int key){
+        return key%size;
+    }
+    void put(int key,int value){
+        int idx=getHashIdx(key);
+        auto &chain=bucket[idx];
+
+        for(auto &it:chain){
+            if(it.first==key){
+                it.second=value;
+                return;
+            }
+        }
+        chain.emplace_back(key,value);
+    }
+    int get(int key){
+        int idx=getHashIdx(key);
+        auto &chain=bucket[idx];
+        if(chain.empty()) return -1;
+
+        for(auto &it:chain){
+            if(it.first==key) return it.second;
+        }
+        return -1;
+    }
+    void remove(int key){
+        int idx=getHashIdx(key);
+        auto &chain=bucket[idx];
+
+        for(auto it=chain.begin();it!=chain.end();it++){
+            if(it->first==key){
+                chain.erase(it);
+                return;
+            }
+        }
+    }
+};
