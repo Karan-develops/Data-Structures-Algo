7f1cd1e0e4bc9ebc9d5ad10c506a36ff9dab607b
Karan <mrkaran2k5@gmail.com>
2025-04-23 15:33:56 +0530

    Count Largest Group

---
+++ Maps/countLargestGrps.cpp
@@ -0,0 +1,34 @@
+// LC - 1399 - Easy - Count Largest Group
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+int countLargestGroup(int n) {
+    unordered_map<int,int>mp;
+    int sum=0,j=0;
+    for(int i=1;i<=n;i++){
+        sum=0,j=i;
+        while(j){
+            sum+=j%10;
+            j/=10;
+        }
+        mp[sum]++;
+    }
+    int ans=0,maxi=0;
+    for(auto &it:mp){
+        if(it.second>maxi){
+            ans=1;
+            maxi=it.second;
+        }
+        else if(it.second==maxi) ans++;
+    }
+    return ans;
+}
+int main(){
+    // Output: 4
+    int n=13;
+    // Output: 2
+    int n2=2;
+    cout<<countLargestGroup(n)<<endl;
+    cout<<countLargestGroup(n2);
+    return 0;
+}
