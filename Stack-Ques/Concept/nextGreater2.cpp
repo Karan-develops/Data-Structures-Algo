a2a681b3cc65d9656bf86bd6026adfd4fc0576f9
Karan <mrkaran2k5@gmail.com>
2025-05-09 20:00:02 +0530

    Next Greater Element II

---
+++ Stack-Ques/Concept/nextGreater2.cpp
@@ -0,0 +1,32 @@
+// LC - 503 - Medium - Next Greater Element II
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+vector<int> nextGreaterElements(vector<int>&nums){
+    int n=nums.size();
+    vector<int>ans(n);
+    stack<int>st;
+
+    for(int i=2*n-1;i>=0;i--){
+        int idx=i%n;
+        if(st.empty()) ans[idx]=-1;
+        else{
+            while(!st.empty() && st.top()<=nums[idx]){
+                st.pop();
+            }
+            st.empty()?ans[idx]=-1:ans[idx]=st.top();
+        }
+        st.push(nums[idx]);
+    }
+    return ans;
+}
+int main(){
+    // Output: [2,-1,2]
+    vector<int>nums={1,2,1};
+    // Output: [2,3,4,-1,4]
+    vector<int>nums2={1,2,3,4,3};
+    vector<int>ans=nextGreaterElements(nums);
+    for(int &i:ans) cout<<i<<" ";
+    return 0;
+}
