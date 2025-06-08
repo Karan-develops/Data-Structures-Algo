c8bb3e3a149f0a39b4256ad7d54715b4c6fdf349
Karan <mrkaran2k5@gmail.com>
2025-06-08 13:34:06 +0530

    Populating Next Right Pointers in Each Node

---
+++ Tree-Q/BFS/populatingNextPointers.cpp
@@ -0,0 +1,47 @@
+// LC - 116 - Medium - Populating Next Right Pointers in Each Node
+#include<iostream>
+#include<queue>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node* next;
+};
+class BFS{
+    public:
+    Node* connect(Node* root) {
+        if(!root) return root;
+
+        queue<Node*>q;
+        q.push(root);
+        Node* prev;
+
+        while(!q.empty()){
+            int N=q.size();
+            prev=NULL;
+            while(N--){
+                Node* curr=q.front();
+                q.pop();
+
+                if(prev) prev->next=curr;
+
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+
+                prev=curr;
+            }
+        }
+        return root;
+    }
+};
+/*
+-> Example 1:
+Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+:-----------------------:
+-> Example 2:
+Input: root = []
+Output: []
+*/
