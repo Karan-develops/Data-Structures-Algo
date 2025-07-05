3f49f40bcce93b851f5a573a4f03c5bb88558c64
Karan <mrkaran2k5@gmail.com>
2025-07-05 12:41:57 +0530

    Merge k Sorted Lists

--- LL-Ques/merge-k-Sorted.cpp
+++ LL-Ques/merge-k-Sorted.cpp
@@ -1,14 +1,15 @@
 // LC - 23 - HARD - Merge k Sorted Lists
+// 2 Approaches - Heap - Recursion
 #include<iostream>
 #include<vector>
 #include<queue>
 using namespace std;
 class Node{
     public:
-    int data;
+    int val;
     Node* next;
-    Node(int data){
-        this->data=data;
+    Node(int val){
+        this->val=val;
         this->next=NULL;
     }
 };
@@ -27,7 +28,7 @@ class Solution{
         priority_queue<int>pq;
         for(auto &i:lists){
             while(i!=NULL){
-                pq.push(i->data);
+                pq.push(i->val);
                 i=i->next;
             }
         }
@@ -37,6 +38,42 @@ class Solution{
             pq.pop();
         }
         return ans;
+    }
+};
+class LL{
+    Node* mergeTwoSortedLists(Node* l1,Node* l2){
+        if(!l1) return l2;
+        if(!l2) return l1;
+
+        if(l1->val<=l2->val){
+            l1->next=mergeTwoSortedLists(l1->next,l2);
+            return l1;
+        } else {
+            l2->next=mergeTwoSortedLists(l1,l2->next);
+            return l2;
+        }
+        return NULL;
+    }
+    
+    Node* partitionAndMerge(int start,int end,vector<Node*>&lists){
+        if(start==end) return lists[start];
+        
+        if(start>end) return NULL;
+        
+        int mid=start+(end-start)/2;
+        
+        Node* l1=partitionAndMerge(start,mid,lists);
+        Node* l2=partitionAndMerge(mid+1,end,lists);
+        
+        return mergeTwoSortedLists(l1,l2);
+    }
+    public:
+    Node* mergeKLists(vector<Node*>&lists){
+        int n=lists.size();
+        
+        if(n==0) return NULL;
+        
+        return partitionAndMerge(0,n-1,lists);
     }
 };
 /*
