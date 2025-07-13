bdde423829cf4c1cc7f3b08331340e0d09113841
Karan <mrkaran2k5@gmail.com>
2025-07-13 18:06:50 +0530

    Power of Four

---
+++ Maths/power4.cpp
@@ -0,0 +1,47 @@
+// LC - 342 - Easy - Power of Four
+// Topics --> Math, Bit Manipulation, Recursion
+#include<iostream>
+#include<math.h>
+using namespace std;
+class PowerFour{
+    public:
+    bool isPowerOfFour(int n){
+        if(n<=0) return false;
+        
+        /*
+        4^a = n
+        a = log(n) to base 4
+        a = log(n) to base e / log(4) to base e
+        */
+        int a=(log(n)/log(4));
+        
+        if(n==pow(4,a)) return true;
+        
+        return false;
+    }
+    bool bitMagic(int n){
+        return n>0 && (n&(n-1))==0 && (n-1)%3==0;
+    }
+    bool recursion(int n){
+        if(n==0) return false;
+        if(n==1) return true;
+
+        if(n%4!=0) return false;
+
+        return recursion(n/4);
+    }
+};
+int main(){
+    // Output: true
+    int n=16;
+    // Output: false
+    int n2=5;
+    // Output: true
+    int n3=1;
+
+    PowerFour sol;
+    cout<<sol.isPowerOfFour(n)<<endl;
+    cout<<sol.isPowerOfFour(n2)<<endl;
+    cout<<sol.isPowerOfFour(n3);
+    return 0;
+}
