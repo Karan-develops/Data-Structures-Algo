66bc8a0ea43f9dbcf5f0e027ed9b81a4b8375c54
Karan <mrkaran2k5@gmail.com>
2025-07-16 15:09:00 +0530

    Add Digits

---
+++ Maths/Concept/addDigit.cpp
@@ -0,0 +1,24 @@
+// LC - 258 - Easy - Add Digits
+#include<iostream>
+using namespace std;
+int addDigits(int num){
+    if(num>=0 && num<=9) return num;
+    
+    int sum=0;
+
+    while(num){
+        int digit=num%10;
+        sum+=digit;
+        num/=10;
+    }
+    return addDigits(sum);
+}
+int main(){
+    // Output: 2
+    int num=38;
+    // Output: 0
+    int num2=0;
+    cout<<addDigits(num)<<endl;
+    cout<<addDigits(num2);
+    return 0;
+}
