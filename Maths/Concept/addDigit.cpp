fcee253c3268bb164e018c6cbf23423963e51535
Karan <mrkaran2k5@gmail.com>
2025-07-31 16:24:25 +0530

    Add Digits

--- Maths/Concept/addDigit.cpp
+++ Maths/Concept/addDigit.cpp
@@ -1,4 +1,5 @@
 // LC - 258 - Easy - Add Digits
+// Follow Up - O(1) Time
 #include<iostream>
 using namespace std;
 int addDigits(int num){
@@ -12,6 +13,13 @@ int addDigits(int num){
         num/=10;
     }
     return addDigits(sum);
+}
+int followUp(int num){
+    if(num==0) return 0;
+
+    if(num%9==0) return 9;
+
+    return num%9;
 }
 int main(){
     // Output: 2
