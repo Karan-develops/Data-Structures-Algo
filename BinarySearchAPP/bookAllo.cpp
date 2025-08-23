c4db01015e6ff742fbee4ac2e0a08017aa4839c8
Karan <mrkaran2k5@gmail.com>
2025-08-23 08:58:45 +0530

    Allocate Minimum Pages

--- BinarySearchAPP/bookAllo.cpp
+++ BinarySearchAPP/bookAllo.cpp
@@ -1,52 +1,61 @@
+// GFG - Medium - Allocate Minimum Pages
 #include<iostream>
+#include<vector>
 #include<numeric>
 using namespace std;
-bool isPossibleSolution(int arr[],int size,int mid,int students){
-    int pageSum=0;
-    int count=1;
-    for(int i=0;i<size;i++){
-        if(arr[i]>mid){
-            return false;
+class BinarySearch{
+    int n;
+    bool isPossibleSolution(vector<int>&arr,int k,int mid){
+        int count=1;
+        int pageSum=0;
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]>mid) return false;
+            
+            if(pageSum+arr[i]>mid){
+                count++;
+                pageSum=arr[i];
+                
+                if(count>k) return false;
+            }
+            else pageSum+=arr[i];
         }
-        if(pageSum+arr[i]>mid){
-            count++;
-            pageSum=arr[i];
-            if(count>students){
-                return false;
+        return true;
+    }
+    public:
+    int findPages(vector<int>&arr,int k){
+        n=arr.size();
+        
+        if(n<k) return -1;
+        
+        int l=0;
+        int r=accumulate(begin(arr),end(arr),0);
+        int ans=-1;
+        
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            
+            if(isPossibleSolution(arr,k,mid)){
+                ans=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
             }
         }
-        else{
-            pageSum+=arr[i];
-        }
+        return ans;
     }
-    return true;
-}
-int bookAllo(int arr[],int students,int size){
-    if(size<students){
-        return -1;
-    }
-    int start=0;
-    int end=accumulate(arr,arr+size,0);
-    int ans=-1;
-    while(start<=end){
-        int mid=start+(end-start)/2;
-        if(isPossibleSolution(arr,size,mid,students)){
-            ans=mid;
-            end=mid-1;
-        }
-        else{
-            start=mid+1;
-        }
-    }
-    return ans;
-}
+};
 int main(){
-    int arr[4]={12,34,67,90};
-    int size=4;
-    int students;
-    cout<<"Enter number of students:";
-    cin>>students;
-    int ans=bookAllo(arr,students,size);
-    cout<<"The correct Allocation is:"<<ans;
+    // Output: 113
+    vector<int>arr={12,34,67,90};
+    int k=2;
+    // Output: -1
+    vector<int>arr2={15,17,20};
+    int k2=5;
+
+    BinarySearch sol;
+    cout<<sol.findPages(arr,k)<<endl;
+    cout<<sol.findPages(arr2,k2);
     return 0;
 }
