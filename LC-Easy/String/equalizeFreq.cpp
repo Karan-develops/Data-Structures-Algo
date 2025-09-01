43260231b0b952535235e40ec606db9c679b077e
Karan <mrkaran2k5@gmail.com>
2025-09-01 21:10:43 +0530

    Remove Letter To Equalize Frequency

--- LC-Easy/String/equalizeFreq.cpp
+++ LC-Easy/String/equalizeFreq.cpp
@@ -3,14 +3,19 @@
 #include<vector>
 #include<set>
 using namespace std;
-bool equalFrequency(string word) {
+bool equalFrequency(string word){
     vector<int>mapping(26,0);
+    
     for(char ch:word) mapping[ch-'a']++;
+
     for(int &val:mapping){
         val--;
+        
         set<int>st;
         for(int &val:mapping) if(val) st.insert(val);
+        
         if(st.size()==1) return true;
+        
         val++;
     }
     return false;
