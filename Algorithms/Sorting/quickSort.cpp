2f380fe7a370dc3f0a9231b7388805e7d106752b
Karan <mrkaran2k5@gmail.com>
2025-09-04 09:26:14 +0530

    Quick Sort

---
+++ Algorithms/Sorting/quickSort.cpp
@@ -0,0 +1,34 @@
+// Quick Sort
+// TC : Average and Best Case: O(nlogn)
+// Worst Case: O(n^2)
+// SC : Average Case: O(log n)
+// Worst Case: O(n)
+#include<iostream>
+#include<vector>
+using namespace std;
+int partition(vector<int>&arr,int low,int high){
+    int pivot=arr[high];
+    int pivotIndex=low;
+
+    for(int i=low;i<high;i++){
+        if(arr[i]<=pivot) swap(arr[i],arr[pivotIndex++]);
+    }
+    
+    swap(arr[high],arr[pivotIndex]);
+    
+    return pivotIndex;
+}
+void quickSort(vector<int>&arr,int low,int high){
+    if(low>=high) return;
+
+    int Pi=partition(arr,low,high);
+    
+    quickSort(arr,low,Pi-1);
+    quickSort(arr,Pi+1,high);
+}
+int main(){
+    vector<int>arr={3,2,1,5,0,3,5,2,1,8,1,6};
+    quickSort(arr,0,arr.size()-1);
+    for(int &i:arr) cout<<i<<" ";
+    return 0;
+}
