d32453eabcd0e4da8833c83fb8f80ae32c62772b
Karan <mrkaran2k5@gmail.com>
2025-12-18 17:03:47 +0530

    Best Time to Buy and Sell Stock using Strategy

--- DP-Ques/Stocks/bestTimeToBuyAndSellStockUsingStrategy.cpp
+++ DP-Ques/Stocks/bestTimeToBuyAndSellStockUsingStrategy.cpp
@@ -2,38 +2,50 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-long long maxProfit(vector<int>&prices,vector<int>&strategy,int k){
-    int n=prices.size();
+class SlidingWindow{
+    typedef long long ll;
+    public:
+    long long maxProfit(vector<int>&prices,vector<int>&strategy,int k){
+        int n=prices.size();
 
-    long long original=0;
-    for(int i=0;i<n;i++){
-        original+=(prices[i]*strategy[i]);
+        ll actualProfit=0;
+        vector<ll>profit(n);
+
+        for(int i=0;i<n;i++){
+            profit[i]=(ll)strategy[i]*prices[i];
+            actualProfit+=profit[i];
+        }
+
+        ll originalWindowProfit=0;
+        ll modifiedWindowProfit=0;
+        ll maxGain=0; // modifiedWindowProfit - originalWindowProfit
+
+        int i=0,j=0;
+
+        while(j<n){
+            originalWindowProfit+=profit[j];
+            
+            // Second half of the window contributes to modifiedWindowProfit
+            if(j-i+1>k/2){
+                modifiedWindowProfit+=prices[j];
+            }
+
+            if(j-i+1>k){
+                originalWindowProfit-=profit[i];
+                modifiedWindowProfit-=prices[i+k/2];
+                i++;
+            }
+
+            if(j-i+1==k){
+                maxGain=max(maxGain,modifiedWindowProfit-originalWindowProfit);
+            }
+
+            j++;
+        }
+
+        return actualProfit+maxGain;
     }
-
-    int half=k/2;
-    long long change=0;
-
-    for(int i=0;i<half;i++){
-        change+=(0*prices[i])-(prices[i]*strategy[i]);
-    }
-    for(int i=half;i<k;i++){
-        change+=(1*prices[i])-(prices[i]*strategy[i]);
-    }
-
-    long long ans=change;
-
-    for(int i=1;i<=n-k;i++){
-        long long leftI=(prices[i-1]*strategy[i-1]);
-        long long midI=(prices[i+half-1]);
-        long long rightI=prices[i+k-1]-(prices[i+k-1]*strategy[i+k-1]);
-
-        change+=leftI-midI+rightI;
-
-        ans=max(ans,change);
-    }
-
-    return original+max(0LL,ans);
-}
+};
 int main(){
     // Output: 10
     vector<int>prices={4,2,8},strategy={-1,0,1};
@@ -41,7 +53,9 @@ int main(){
     // Output: 9
     vector<int>prices2={5,4,3},strategy2={1,1,0};
     int k2=2;
-    cout<<maxProfit(prices,strategy,k)<<endl;
-    cout<<maxProfit(prices2,strategy2,k2);
+
+    SlidingWindow sol;
+    cout<<sol.maxProfit(prices,strategy,k)<<endl;
+    cout<<sol.maxProfit(prices2,strategy2,k2);
     return 0;
 }
