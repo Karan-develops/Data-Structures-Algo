88bcd6bd19ef18e9df785cc7f4f9b0fc999961fa
Karan <mrkaran2k5@gmail.com>
2026-01-03 11:43:05 +0530

    Number of Ways to Paint N × 3 Grid

---
+++ DP-Ques/Maths/numberWaysPaintN×3Grid.cpp
@@ -0,0 +1,21 @@
+// LC - 1411 - HARD - Number of Ways to Paint N × 3 Grid
+#include<iostream>
+#include<vector>
+using namespace std;
+class DP{
+    public:
+    int numOfWays(int n){
+        // TODO:
+    }
+};
+int main(){
+    // Output: 12
+    int n=1;
+    // Output: 30228214
+    int n2=5000;
+
+    DP sol;
+    cout<<sol.numOfWays(n)<<endl;
+    cout<<sol.numOfWays(n2);
+    return 0;
+}
