35e432409298745550078d84c07f046b8d322b54
Karan <mrkaran2k5@gmail.com>
2026-01-15 11:14:10 +0530

    Maximize Area of Square Hole in Grid

--- Sorting-Array-Q/maxAreaSquareHoleGrid.cpp
+++ Sorting-Array-Q/maxAreaSquareHoleGrid.cpp
@@ -4,7 +4,41 @@
 #include<algorithm>
 using namespace std;
 int maximizeSquareHoleArea(int n,int m,vector<int>&hBars,vector<int>&vBars){
+    sort(begin(hBars),end(hBars));
+    sort(begin(vBars),end(vBars));
 
+    int maxConsecutiveHBars=1;
+    int maxConsecutiveVBars=1;
+
+    // First find Length of Longest consecutive horizontal bars
+    int currConsecutiveHBars=1;
+
+    for(int i=1;i<hBars.size();i++){
+        if(hBars[i]-hBars[i-1]==1){
+            currConsecutiveHBars++;
+        } else{
+            currConsecutiveHBars=1;
+        }
+        maxConsecutiveHBars=max(maxConsecutiveHBars,currConsecutiveHBars);
+    }
+
+    // Similarly, find Length of Longest consecutive vertical bars
+    int currConsecutiveVBars=1;
+
+    for(int i=1;i<vBars.size();i++){
+        if(vBars[i]-vBars[i-1]==1){
+            currConsecutiveVBars++;
+        } else{
+            currConsecutiveVBars=1;
+        }
+        maxConsecutiveVBars=max(maxConsecutiveVBars,currConsecutiveVBars);
+    }
+
+    // Square width and height must be same. We can reduce the larger one to match the smaller one
+    // So, take the minimum of them.
+    int side=min(maxConsecutiveHBars,maxConsecutiveVBars)+1;
+    
+    return side*side;
 }
 int main(){
     // Output: 4
